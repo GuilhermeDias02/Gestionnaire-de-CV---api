373b3c10deb54ae0d143e6493f97fb2337721eb9
GuilhermeDias02 <guilherme.alex.dias@gmai.com>
2024-11-25 17:31:39 +0100

    fin crud cv

--- controllers/cv.js
+++ controllers/cv.js
@@ -62,7 +62,7 @@ module.exports = {
             res.status(201);
             res.send({
                 success: true,
-                book: newCv,
+                cv: newCv,
             });
         } catch (error) {
             res.status(500);
@@ -71,4 +71,56 @@ module.exports = {
             });
         }
     },
+
+    editCv: async (req, res) => {
+        try {
+            const cvId = req.params.id;
+
+            const isNotValidate = verifyCv(req.body);
+            if (isNotValidate) {
+                res.status(400);
+                res.send({
+                    error: isNotValidate.message
+                });
+            }
+
+            const updatedCv = await Cv.findByIdAndUpdate(cvId, req.body, {
+                new: true
+            });
+
+            if (!updatedCv) {
+                return res.status(404).send({
+                    message: `Cv with id=${cvId} not found.`
+                });
+            }
+
+            res.send({
+                message: 'Cv was updated successfully.',
+                data: updatedCv
+            });
+        } catch (error) {
+            res.status(500).send({
+                message: error.message || `Error updating cv with id=${req.params.id}`
+            });
+        }
+    },
+
+    deleteCv: async (req, res) => {
+        try {
+            const cvId = req.params.id;
+            const cv = await Cv.findByIdAndDelete(cvId);
+
+            if (cv) {
+                res.status(204).send({
+                    message: 'Cv has been removed successfully.'
+                });
+            } else {
+                res.status(400).send(`No record with given id: ${cvId}`);
+            }
+        } catch (error) {
+            res.status(500).send({
+                message: error.message || `Error deleting cv with id=${req.params.id}`
+            });
+        }
+    }
 };
