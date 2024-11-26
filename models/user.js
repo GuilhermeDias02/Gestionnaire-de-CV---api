8f33857cbec2d4a379dd2c199dae543d0f1294c3
GuilhermeDias02 <guilherme.alex.dias@gmai.com>
2024-11-26 11:31:39 +0100

    user validator

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
         prenom: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        role: { type: String, enum: ["admin", "user"], required: true },
+        role: { type: String, enum: ["admin", "user"], default: 'user'},
     },
     { timestamps: true }
 );
