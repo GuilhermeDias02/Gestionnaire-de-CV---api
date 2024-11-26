019ae4d5006308287e51ff68d6ab5a97fa1cb124
rayane rostane <rayane.rostane@universite-paris-saclay.fr>
2024-11-26 12:50:59 +0100

    Api casi terminé

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,3 +5,5 @@ const authController = require("../controllers/auth");
 // Route pour créer un utilisateur
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/user')
+const userController = require('../controllers/user');
+const User = require('../models/user'); 
 
 // Route pour récupérer tous les utilisateurs
 router.get('/', async (req, res) => {
@@ -14,21 +15,24 @@ router.get('/', async (req, res) => {
 
 // Route pour créer un utilisateur
 router.post('/', async (req, res) => {
-  console.log('Données reçues dans la requête POST:', req.body);
+  //console.log('Données reçues dans la requête POST:', req.body);
   try {
-    const { nom, prenom, email, password, role="user" } = req.body; //un utilisateur a pour le moment le role user automatiquement (on verra par la suite pour le role admin)
 
+    const { nom, prenom, email, password, role="user" } = req.body; //un utilisateur a pour le moment le role user automatiquement (on verra par la suite pour le role admin)
+    console.log('Données reçues dans la requête POST test:', req.body);
     // Validation des champs
     if (!nom || !prenom || !email || !password) {
       return res.status(400).json({ message: 'Tous les champs sont obligatoires' });
     }
 
     // Vérifier si l'email existe déjà
+    console.log('test1');
     const existingUser = await User.findOne({ email });
+    console.log('test2');
     if (existingUser) {
       return res.status(400).json({ message: 'Cet email est déjà utilisé' });
     }
-
+    console.log('Avant création');
     // Création d'un nouvel utilisateur
     const newUser = new User({
       nom,
@@ -37,7 +41,7 @@ router.post('/', async (req, res) => {
       password, 
       role
     });
-
+    console.log('Après création');
     // Sauvegarde dans MongoDB
     const savedUser = await newUser.save();
     res.status(201).json(savedUser); 
@@ -45,7 +49,7 @@ router.post('/', async (req, res) => {
     res.status(500).json({ message: 'Erreur lors de la création de l\'utilisateur', error });
   }
 });
-router.get("/", userController.getAll);// juste pour les tests
-router.get("/:id", userController.getOne);
+//router.get("/", userController.getAll);// juste pour les tests
+//router.get("/:id", userController.getOne);
 
 module.exports = router;
